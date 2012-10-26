a37d7ee002b024b436f1add801b91ae6d0f87220
Fred Wenzel <fwenzel@mozilla.com>
2012-10-26 15:14:33 -0700

    some prototyping, but now we need to wait until the APIs exist. Uuuh.

--- README.md
+++ README.md
@@ -3,11 +3,23 @@ noodletune
 
 An open web app instrument tuner.
 
+*NOTE*: This does not work yet, until Audio support lands in Firefox.
+
+
+Usage
+-----
+* In Firefox 19 (Nightly), browse to ``about:config`` and switch
+  ``media.navigator.enabled`` to ``true`` to enable the getUserMedia API.
+* Then wait a little until the API is implemented in Firefox...
+* ... and continue writing this app.
+
+
 Thanks
 ------
 * Edna Piranha for Noogle Enterprises.
 * Vlademareous for the [Violin icon](http://findicons.com/icon/77094/violin)
   (CC-by licensed)
+
 
 License
 -------
--- www/js/app.js
+++ www/js/app.js
@@ -1,30 +1,55 @@
-
-// This uses require.js to structure javascript:
-// http://requirejs.org/docs/api.html#define
-
 define(function(require) {
-    // Zepto provides nice js and DOM methods (very similar to jQuery,
-    // and a lot smaller):
-    // http://zeptojs.com/
     var $ = require('zepto');
 
-    // Need to verify receipts? This library is included by default.
-    // https://github.com/mozilla/receiptverifier
-    require('receiptverifier');
+    // getUserMedia browser fallback
+    navigator.getMedia = (navigator.getUserMedia ||
+                          navigator.webkitGetUserMedia ||
+                          navigator.mozGetUserMedia ||
+                          navigator.msGetUserMedia);
+    var mediaStream;
 
     // Want to install the app locally? This library hooks up the
     // installation button. See <button class="install-btn"> in
     // index.html
-    require('./install-button');
+    //require('./install-button');
 
-    // Write your app here.
+    function mediaended() {
+        // clean up after we end the recording
+        $('#listen')
+            .removeClass('on')
+            .text('Listen');
+    }
 
+    /** Analyze user media stream for fundamental frequency. */
+    function fft(stream) {
+        mediaStream = stream;
+        mediaStream.onended = mediaended;
 
+        console.log('omg streaming');
 
+        var track = stream.videoTracks;
+        console.log(stream.label);
+    }
 
+    $('#listen').click(function(e) {
+        e.preventDefault();
 
+        if ($(this).hasClass('on')) {
+            mediaended();
+            mediaStream.stop();
+        } else {
+            navigator.getMedia({video: false, audio: true}, function(stream) {
+                console.log('listening!');
+                $('#listen')
+                    .addClass('on')
+                    .text('Stop Listening');
+                fft(stream);
 
+            }, function(err) {
+                console.log("The following error occured: " + err);
+            });
+        }
+    });
 
 
 });
-
